ef6e24b97f59680c936dea3d741621314194891b
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-31 17:10:44 +0700

    .ignoreとcounterApp

--- counter/ContentView.swift
+++ counter/ContentView.swift
@@ -8,13 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var isShowAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                
+                if count < 0 {
+                    Text("\(count)")
+                        .foregroundStyle(.red)
+                        .font(.system(size: 130, weight:.bold,design: .default))
+                } else {
+                    Text("\(count)")
+                        .foregroundStyle(.black)
+                        .font(.system(size: 130, weight:.bold,design: .default))
+                }
+                
+//                Text("\(count)")
+//                    .font(.system(size: 130, weight: .bold, design: .default))
+                
+                HStack{
+                    Button{
+                        count -= 1
+                    } label: {
+                        Text("-")
+                            .frame(height: UIScreen.main.bounds.height/2.5)
+                            .frame(maxWidth:.infinity)
+                            .background(Color("MinusButonColor"))
+                            .foregroundColor(.white)
+                            .font(.system(size: 130, weight:.bold, design:.default))
+                            .cornerRadius(10)
+                    }
+                    
+                    Button{
+                        count += 1
+                    } label: {
+                        Text("+")
+                            .frame(height: UIScreen.main.bounds.height/2.5)
+                            .frame(maxWidth:.infinity)
+                            .background(Color("PlusButtonColor"))
+                            .foregroundColor(.white)
+                            .font(.system(size: 130, weight:.bold, design:.default))
+                            .cornerRadius(10)
+                    }
+                }
+                Spacer()
+                
+                Button{
+                    isShowAlert = true
+                } label: {
+                    Text("リセットする")
+                        .bold()
+                        .frame(maxWidth:.infinity)
+                        .frame(height:50)
+                        .background(.white)
+                        .foregroundColor(.red)
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(.red, lineWidth:1))
+                }
+                .alert("注意", isPresented:$isShowAlert){
+                    Button("リセットする",role:.destructive){
+                        count = 0
+                    }
+                    Button("キャンセル",role:.cancel){}
+                } message: {
+                    Text("現在のカウントをリセットしますが、本当によろしいでしょうか？")
+                }
+            }
         }
+        
         .padding()
     }
 }
