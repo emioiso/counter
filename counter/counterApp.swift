7573c84304c587429eb2a70c445571a44e75bc3f
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-31 14:18:52 +0700

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by emi oiso on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
